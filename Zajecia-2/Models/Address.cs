34597e1886a60df7bb4aaf3fd103451ff90d15de
Mateusz Lisowski <matilisek16@wp.pl>
2021-03-16 09:50:35 +0100

    Dodaj pliki projektów.

---
+++ Zajecia-2/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zajecia._2.Models
+{
+    public class Address
+    {
+        [Display(Name = "Nazwa Twojej Ulicy")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string Street { get; set; }
+        [Display(Name = "Twoje miasto")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Kod pocztowy")]
+        public string City { get; set; }
+        [Display(Name = "Numer domu")]
+        public int Number { get; set; }
+    }
+
+}
